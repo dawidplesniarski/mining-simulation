0f05a49d3df05aadd8b730e22dbf7414f2c81fd2
dawidplesniarski <dawidplesniarski@gmail.com>
2021-12-07 23:17:07 +0100

    miners network generating and deleting when lower than 0

--- src/App.js
+++ src/App.js
@@ -26,15 +26,16 @@ const OverviewFlow = () => {
             data: {
                 label: (
                     <>
-                        <strong>Górnik 1 </strong>
-                        Wartość: 500
+                        <strong>Górnik 1</strong>
+                        Wartość: 800
+                        Moc obliczeniowa: 500
                     </>
                 ),
             },
             position: {x: 400, y: 200},
             actualValue: 0,
-            power:800,
-            value:500,
+            power: 800,
+            value: 500,
             style: {
                 background: '#a2a1c4',
                 color: '#333',
@@ -42,6 +43,24 @@ const OverviewFlow = () => {
                 width: 180,
             }
         },
+        {
+            id: 'e1-2',
+            source: '1',
+            target: '2',
+            label: 'e1-2'
+        },
+        {
+            id: 'e1-3',
+            source: '1',
+            target: '3',
+            label: 'e1-3'
+        },
+        {
+            id: 'e1-4',
+            source: '1',
+            target: '4',
+            label: 'e1-4'
+        }
     ]);
 
     function getRandomInt(min, max) {
@@ -58,10 +77,12 @@ const OverviewFlow = () => {
      */
     const addMiner = () => {
         const newId = Number.parseInt(elements.filter(e => !e.target)[elements.filter(e => !e.target).length - 1].id, 10) + 1;
-        const xPosition = elements.filter(e => !e.target)[elements.filter(e => !e.target).length - 1].position.x + 70;
-        const yPosition = elements.filter(e => !e.target)[elements.filter(e => !e.target).length - 1].position.y + 70;
-        const randomValue = getRandomInt(5, 20)*10 ;
-        const randomPower = getRandomInt(1,6)*100 ;
+        // const xPosition = elements.filter(e => !e.target)[elements.filter(e => !e.target).length - 1].position.x + 70;
+        // const yPosition = elements.filter(e => !e.target)[elements.filter(e => !e.target).length - 1].position.y + 70;
+        const xPosition = getRandomInt(-500, 500);
+        const yPosition = getRandomInt(-500, 500);
+        const randomValue = getRandomInt(5, 20) * 10;
+        const randomPower = getRandomInt(1, 6) * 100;
         const miner = {
             id: newId.toString(),
             type: 'input',
@@ -70,8 +91,8 @@ const OverviewFlow = () => {
                 label: (
                     <>
                         <strong>Górnik {newId} </strong>
-                        Wartość: {randomValue} 
-                        Moc obliczeniowa: {randomPower }
+                        Wartość: {randomValue}
+                        Moc obliczeniowa: {randomPower}
                     </>
                 ),
             },
@@ -85,21 +106,34 @@ const OverviewFlow = () => {
                 width: 180,
             }
         }
-        const edge = {
-            id: `e${elements.filter(e => !e.target)[elements.filter(e => !e.target).length - 1].id}-${newId}`,
-            source: `${elements.filter(e => !e.target)[elements.filter(e => !e.target).length - 1].id}`,
-            target: `${newId}`,
-            label: ''
+        const edge1 = {
+            id: `e${newId}-${newId + 1}`,
+            source: `${newId}`,
+            target: `${newId + 1}`,
+            label: `e${newId}-${newId + 1}`
+        };
+        const edge2 = {
+            id: `e${newId}-${newId + 2}`,
+            source: `${newId}`,
+            target: `${newId + 2}`,
+            label: `e${newId}-${newId + 2}`
+        };
+        const edge3 = {
+            id: `e${newId}-${newId + 3}`,
+            source: `${newId}`,
+            target: `${newId + 3}`,
+            label: `e${newId}-${newId + 3}`
         };
 
-        setElements(oldArr => [...oldArr, miner, edge]);
+        setElements(oldArr => [...oldArr, miner, edge1, edge2, edge3]);
     }
 
     /*
     Usuwa górnika o podanym ID i tworzy węzeł między poprzednim a następnym górnikiem
      */
     const deleteMiner = (id) => {
-        setElements(elements.filter( e => e.id !== id.toString() ));
+        console.log('delete func call');
+        setElements(elements.filter(e => e.id !== id.toString()));
         const edge = {
             id: `e${id - 1}-${id + 1}`,
             source: `${id - 1}`,
@@ -108,70 +142,73 @@ const OverviewFlow = () => {
         };
         setElements(oldArr => [...oldArr, edge]);
     }
-    const updateMiners = () =>{
-      const lastNode = elements.slice(-2,-1)
-      const nodeQuantity = lastNode[0].id
-      let cost = 0;
-      let ifcost = false;
-      setElements((els)=>els.map((el)=>{
-          el.actualValue +=el.power
-          if(ifcost === true) {
-              el.value = el.value - 50;
-              cost = cost + 1;
-              el.data ={...el.data, label:(
-                  <>
-                      <strong>Górnik {el.id} </strong>
-                      Wartość: {el.value} 
-                      Moc obliczeniowa: {el.power }
-                  </>
-              )}
-          }
-          if(cost === elements.length-1){
-              cost = 0;
-              ifcost = false;
-          }
-          if(el.actualValue>=networkValue) {
-              ifcost = true;
-              el.style ={...el.style, background:'red'};
-              el.value = el.value + 200
-              el.actualValue = 0;
-              el.data ={...el.data, label:(
-                  <>
-                      <strong>Górnik {el.id} </strong>
-                      Wartość: {el.value} 
-                      Moc obliczeniowa: {el.power }
-                  </>
-              )}
-              
-          }
+    const updateMiners = () => {
+        const lastNode = elements.slice(-2, -1)
+        const nodeQuantity = lastNode[0].id
+        let cost = 0;
+        let ifcost = false;
+        setElements((els) => els.map((el) => {
+            el.actualValue += el.power
+            if (ifcost === true) {
+                el.value = el.value - 50;
+                cost = cost + 1;
+                el.data = {
+                    ...el.data, label: (
+                        <>
+                            <strong>Górnik {el.id} </strong>
+                            Wartość: {el.value}
+                            Moc obliczeniowa: {el.power}
+                        </>
+                    )
+                }
+            }
+            if (cost === elements.length - 1) {
+                cost = 0;
+                ifcost = false;
+            }
+            if (el.actualValue >= networkValue) {
+                ifcost = true;
+                el.style = {...el.style, background: 'red'};
+                el.value = el.value + 200
+                el.actualValue = 0;
+                el.data = {
+                    ...el.data, label: (
+                        <>
+                            <strong>Górnik {el.id} </strong>
+                            Wartość: {el.value}
+                            Moc obliczeniowa: {el.power}
+                        </>
+                    )
+                }
 
+            }
+            return el;
+        }))
 
-          return el;
-
-          
-          
-      }))
-      for(let i = 1; i<nodeQuantity; i++) {
-          if(elements[i].value <= 0){
-                  const id = i;
-                  const preEdge = `e${id - 1}-${id}`
-                  const afterEdge = `e${id}-${id + 1}`
-                  setElements(elements.filter( e => e.id !== id.toString() ));
-                  setElements(elements.filter( e => e.id !== preEdge))
-                  setElements(elements.filter( e => e.id !== afterEdge))
-                  const edge = {
-                      id: `e${id - 1}-${id + 1}`,
-                      source: `${id - 1}`,
-                      target: `${id + 1}`,
-                      label: ''
-                  };
-                  setElements(oldArr => [...oldArr, edge]);
-              }
-          }
-      
-      console.log(elements)
-      //console.log(lastNode)
-  }
+        for (let i = 1; i < nodeQuantity; i++) {
+            if (elements[i].value <= 0) {
+                const id = i;
+                const preEdge = `e${id - 1}-${id}`
+                const afterEdge = `e${id}-${id + 1}`
+                setElements(elements.filter(e => e.id !== id.toString()));
+                setElements(elements.filter(e => e.id !== preEdge))
+                setElements(elements.filter(e => e.id !== afterEdge))
+                const edge = {
+                    id: `e${id - 1}-${id + 1}`,
+                    source: `${id - 1}`,
+                    target: `${id + 1}`,
+                    label: ''
+                };
+                setElements(oldArr => [...oldArr, edge]);
+            }
+        }
+        elements.forEach(el => {
+            if (el.value <= 0) {
+                deleteMiner(el.id);
+            }
+        })
+        console.log(elements)
+    }
 
     return (
         <MainView>
@@ -186,9 +223,9 @@ const OverviewFlow = () => {
                 </Button>
             </ButtonWrapper>
             <ButtonWrapper>
-            <Button onClick={() => updateMiners()}>
-                Aktualizuj
-            </Button>
+                <Button onClick={() => updateMiners()}>
+                    Aktualizuj
+                </Button>
             </ButtonWrapper>
             <GraphScene elements={elements} setElements={setElements}/>
         </MainView>
