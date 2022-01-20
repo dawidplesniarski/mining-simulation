223a7fabbc31c4042c291e476051e204d89f9076
dawidplesniarski <dawidplesniarski@gmail.com>
2022-01-20 19:29:50 +0100

    Move BlockView and Miners view to MainPage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React from 'react';
-import GraphScene from "./components/molecules/GraphScene";
-import playIcon from './assets/play-button.png';
-import stopIcon from './assets/stop-button.png';
-import BlocksGraphScene from "./components/molecules/BlocksGraphScene";
 import MainPage from "./pages/MainPage";
 
 const OverviewFlow = () => {
-
 
     return (
         <MainPage />
